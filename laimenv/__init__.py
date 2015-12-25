acf477d99147bb292c2c442f7cc2f61e48da8485
Beshr Kayali <beshrkayali@gmail.com>
2015-12-25 09:46:55 +0100

    Init commands

--- laimenv/__init__.py
+++ laimenv/__init__.py
@@ -1,7 +1,35 @@
-import uuid
 from pylxd import api
 
-lxd = api.API()
 
-class Laim(object):
-    pass
+class LaimLaim(object):
+    SOURCE_TYPE = (
+        'image',
+        'migration',
+        'copy',
+        'none',
+    )
+
+    def __init__(self):
+        self.lxd = api.API()
+
+    def envs(self):
+        containers = self.lxd.list_containers()
+        return containers
+
+    def startenv(self, name, source_type, source_alias,
+                 profiles, conf, ephemeral=False):
+
+        container = self.lxd.container_init(
+            {
+                'name': name,
+                'architecture': 2,
+                'profiles': profiles,
+                'ephemeral': ephemeral,
+                'source': {
+                    'type': source_type,
+                    'alias': source_alias
+                }
+            }
+        )
+
+        return container
--- laimtool.py
+++ laimtool.py
@@ -1,6 +1,34 @@
 import click
+from laimenv import LaimLaim
+laim = LaimLaim()
 
-@click.command()
+
+@click.group()
 def cli():
-    click.echo("Hello!")
+    click.echo("Run a command!")
     pass
+
+
+@cli.command()
+def envs():
+    click.echo(['- %s\n' % env for env in laim.envs()])
+
+
+@cli.command()
+@click.option('--env_name', prompt='Env name',
+              help='Give a name to your environment')
+@click.option('--source_type', prompt='Source type',
+              help='Choose between: ' + ' '.join(
+                  st for st in laim.SOURCE_TYPE))
+@click.option('--source_alias', prompt='Source alias',
+              help="If it's an image, type in the alias name of the image")
+def startenv(env_name, source_type, source_alias):
+    c = laim.startenv(
+        name=env_name,
+        source_type='image',
+        source_alias=source_alias,
+        profiles=['default'],
+        conf={},
+        ephemeral=False
+    )
+    click.echo(c)
